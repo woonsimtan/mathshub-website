791798b74a580bd0a070844f7027ab1aaebccb1d
WoonSim Tan <tan.woonsim@outlook.com>
2022-06-30 12:33:40 +0100

    cleanup some default react stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React from 'react';
@@ -13,18 +12,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WIP:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Mathshub Website
+        </h1>
       </header>
       <button onClick={hitBackend}>Send request</button>
     </div>
