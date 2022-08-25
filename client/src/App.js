327b9ffc9e2dde3dff3a052f80190bb801f35f69
Woon Sim Tan <70201781+woonsimtan@users.noreply.github.com>
2022-08-25 16:44:55 +0100

    Revert "Add header footer"

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Footer from './components/footer';
 import Navbar from './components/navbar';
 import About from "./pages/About"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
-import Header from './components/header';
 
 function App() {
   
   return (
     <div>
-    <Header/>
 
     <Router>
         <Navbar />
@@ -20,9 +17,7 @@ function App() {
             <Route path='/about' element={<About/>} />
             <Route path='/login' element={<Login/>} />
         </Routes>
-    </Router>
-
-    <Footer/>
+        </Router>
 
       
     </div>
